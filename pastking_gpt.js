3dfd990fbc04d03f4b18ff0dfbc202d3dec024fb
anime <street-anime-olive@duck.com>
2024-11-06 03:01:38 +0800

    fix(修复未携带token的问题):

--- pastking_gpt.js
+++ pastking_gpt.js
@@ -153,7 +153,7 @@ function ChucklePostAI(AI_option) {
       const title = document.title;
       
       try {
-        const response = await fetch('https://summary.qixing1217.top/api/summary', {
+        const response = await fetch('https://summary.qixing1217.top/api/summary?token=57X8Ht6R9a8GX548ggS', {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
